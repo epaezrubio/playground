bdda9e1d8b2a7034ac1199d46c8db0ce8477e791
Albert Lacambra <albert.lacambra@ion2s.com>
2014-02-11 19:10:28 +0100

    more testing

--- testForms/app.js
+++ testForms/app.js
@@ -1,16 +1,26 @@
 var main = angular.module("main", []);
 
 main.controller("FormCtrl", function($scope, $http) {
+    var endpoint = "http://localhost:8080/rest/";
 	$scope.formData = {};
-	$scope.action = $scope.$watch("action")
 	$scope.submit = function() {
 		$http({
 	        method: 'POST',
-	        url: $scope.action,
+	        url: endpoint + $scope.action,
 	        data: $scope.formData,
-	        headers: { 'Content-Type': 'application/x-www-form-urlencoded' } 
-	    }).success(function() {
+	        headers: { 'Content-Type': 'application/json',
+	        			'Authorization': 'Basic YUBhLmNvbTph'}
+	    }).success(function(data) {
 	    	console.log("Data was sent");
+               $scope.resource =  data;
 	    });
-	}
+    }
+    $scope.getData = function() {
+        return $http({
+            method: 'GET',
+            url: endpoint + $scope.action,
+        }).success(function(data) {
+            $scope.output =  data; 
+        }); 
+    }
 });
